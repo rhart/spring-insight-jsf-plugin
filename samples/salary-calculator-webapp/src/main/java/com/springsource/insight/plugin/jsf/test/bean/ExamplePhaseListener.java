0a833519e0d97c97dc1f41d40ed3a35baecf18a1
Gareth Webbley <gareth.webbley@infinite-software-ltd.com>
2012-11-25 15:32:36 +0000

    Adding Phase Listener aspect plus changing other aspected so they are not MyFaces dependent

---
+++ samples/salary-calculator-webapp/src/main/java/com/springsource/insight/plugin/jsf/test/bean/ExamplePhaseListener.java
@@ -0,0 +1,22 @@
+package com.springsource.insight.plugin.jsf.test.bean;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class ExamplePhaseListener implements PhaseListener {
+
+    private static final long serialVersionUID = 20121125L;
+
+    public void beforePhase(PhaseEvent event) {
+        System.out.println("In before phase");
+    }
+
+    public void afterPhase(PhaseEvent event) {
+        System.out.println("In after phase");
+    }
+
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+}
